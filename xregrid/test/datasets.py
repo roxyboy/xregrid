8427207c5caf31af30d04ae4aa129eec09c75e87
Takaya Uchida <takaya@ldeo.columbia.edu>
2017-03-17 17:38:33 -0400

    edited test

---
+++ xregrid/test/datasets.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from future.utils import iteritems
+import pytest
+import xarray as xr
+import numpy as np
+import dask.array as da
+
+
+x = np.linspace(0., 2*np.pi, 100)
+y = np.linspace(-10., 10., 50)
+xx, yy = np.meshgrid(x, y)
+indexx = range(len(x))
+indexy = range(len(y))
+data = np.sin(x) + y[:, np.newaxis]
+
+
+dataarrays = {
+    'd2d' : xr.DataArray(data[np.newaxis, :, :], 
+                         dims=['time', 'ny', 'nx'], 
+                         coords={'time': range(1), 'ny': indexy, 'nx': indexx, 
+                                 'yy': (('ny', 'nx'), yy), 
+                         'xx': (('ny', 'nx'), xx)}
+                        ).to_dataset(name='f')
+}
+
+@pytest.fixture(params=['d2d'])
+def d2d(request):
+    return dataarrays[request.param]
--- xregrid/test/test_regrid.py
+++ xregrid/test/test_regrid.py
@@ -8,3 +8,16 @@ import pandas as pd
 import os
 
 
+from . datasets import d2d
+
+def test_length_args(d2d):
+    varname = d2d.data_vars.keys()[0]
+    nx = d2d.coords.keys()[0]
+    ny = d2d.coords.keys()[1]
+    xx = d2d.coords.keys()[2]
+    yy = d2d.coords.keys()[3]
+    time = d2d.coords.keys()[4]
+    args = [time, ny, nx]
+   
+    with pytest.raises(AssertionError):
+        xg.stack_var(d2d, xx, yy, True, False, *args)
